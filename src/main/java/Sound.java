815877950f9d6f5ec5ae5483a4f72d8212ab3cd2
willy2day <143228693+willy2day@users.noreply.github.com>
2024-04-15 22:40:23 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -17,7 +17,7 @@ public class Sound
   {  
     /* to be implemented in part (a) */
     int count =0;
-    for (int i =0; i<samples.length(); i++){
+    for (int i =0; i<samples.length; i++){
       if (samples[i]>limit){
         samples[i]=limit;
         count++;}
